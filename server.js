48e86bbb168e1c1f9455c036582490b4cbeea5b2
Matt69-420 <90251612+Matt69-420@users.noreply.github.com>
2021-10-06 15:16:05 +0300

    wwww

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-console.log("see töötas")
+const http = require('http');
+
+const requestListener = function (req, res) {
+    console.log(reg.url)
+
+    const requestListener = function (req, res) {
+      console.log(req.url)
+  
+      let search = req.url.split("?")
+      console.log(search)
+      let parameters = search.split('=')
+      console.log(parameters)
+      let key = parameters[0]
+      let value = parameters[1]
+      console.log(key, value)
+      let sql = "INSERT INTO players (name) VALUES ('"+value+"');"
+      console.log(sql)
+  
+      res.writeHead(200);
+      res.end('<b>Hello, World!</b> Tulid: '+req.url);
+  res.writeHead(200);
+  res.end('Hello, World!');
+}
+
+const server = http.createServer(requestListener);
+server.listen(8080);
